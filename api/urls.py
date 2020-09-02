39fc97629576ce7f11c9f4117cfb0011ce5f1063
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2020-09-02 19:51:06 +0100

    Fixed typo

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('athletes/', Athletes.as_view(), name="athletes"),
-    # path('athletes/<int:id>/', Athletes.as_view(), name="athletes"),
+    path('athletes/<int:id>/', Athletes.as_view(), name="athletes"),
     path('athletes_listview/', ListAthletesView.as_view(), name="listview"),
     path('athletes_listview/<int:id>', ListAthletesView.as_view(), name="listview"),
     path('athletes_html/', views.index, name="webpage")
